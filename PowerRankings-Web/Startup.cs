b8b0d0d44b58956f84d351b001bdb8242e4025e1
Ryan Torello <ryan.torello@gmail.com>
2018-01-21 21:30:51 -0500

    added MVC layer

--- PowerRankings-Web/Startup.cs
+++ PowerRankings-Web/Startup.cs
@@ -15,6 +15,7 @@ namespace PowerRankings_Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +25,17 @@ namespace PowerRankings_Web
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                routes.MapRoute(
+                "Default",                                              // Route name
+                "{controller}/{action}/{id}",                           // URL with parameters
+                new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
+            );
+
+            }
+            );
         }
     }
 }
