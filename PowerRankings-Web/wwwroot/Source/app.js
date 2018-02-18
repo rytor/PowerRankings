3a8298e83a6ff706b9e35aa70e0c82b6978ffd69
Ryan Torello <ryan.torello@gmail.com>
2018-02-18 16:17:20 -0500

    Added Bootstrap, Babel, other plugins

--- PowerRankings-Web/wwwroot/Source/app.js
+++ PowerRankings-Web/wwwroot/Source/app.js
@@ -1,2 +1,7 @@
 ﻿require('./lib');
+import ES6Lib from './es6codelib';
+import 'bootstrap/dist/css/bootstrap.min.css';
 document.getElementById("fillthis").innerHTML = getText();
+$('#fillthiswithjquery').html('Filled by Jquery!');
+let myES6Object = new ES6Lib();
+$('#fillthiswithes6lib').html(myES6Object.getData());
