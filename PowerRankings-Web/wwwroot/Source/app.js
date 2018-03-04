ac87c456df7e27a2c3d535bf2a49614f3ae1fe2d
rytor <32495898+rytor@users.noreply.github.com>
2018-03-03 20:45:56 -0500

    Epic/web/init (#3)
    * initial web commit
    
    * added MVC layer
    
    * added basic javascript
    
    * added webpack and config
    
    * Added Bootstrap, Babel, other plugins
    
    * Added React, it's go time
    
    * moved sample files to sample folder

---
+++ PowerRankings-Web/wwwroot/Source/app.js
@@ -0,0 +1,14 @@
+﻿require('./lib');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import Counter from './test';
+//import ES6Lib from './es6codelib';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//ReactDOM.render(
+//    <Counter />,
+//    document.getElementById('basicreactcomponent')
+//);
+
+module.hot.accept();
