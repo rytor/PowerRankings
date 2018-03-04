278c8c0ddb02f9c8f08fc67d90f004591343e797
Ryan Torello <ryan.torello@gmail.com>
2018-03-03 20:45:13 -0500

    moved sample files to sample folder

--- PowerRankings-Web/wwwroot/Source/app.js
+++ PowerRankings-Web/wwwroot/Source/app.js
@@ -2,13 +2,13 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './test';
-import ES6Lib from './es6codelib';
+//import Counter from './test';
+//import ES6Lib from './es6codelib';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-    <Counter />,
-    document.getElementById('basicreactcomponent')
-);
+//ReactDOM.render(
+//    <Counter />,
+//    document.getElementById('basicreactcomponent')
+//);
 
 module.hot.accept();
